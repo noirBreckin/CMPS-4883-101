b31d3c9dec295089d6d32a3aa700939485ffa926
noirBreckin <112154255+noirBreckin@users.noreply.github.com>
2024-09-05 14:30:23 -0500

    Create main.cpp

---
+++ Assignments/A03/main.cpp
@@ -0,0 +1,20 @@
+/**
+* Name: Breckin Hume
+* Course: 4883-Prog Tech
+* Date: 9-6-24
+*/
+#include <iostream>
+
+#define endl "\n"
+
+using namespace std;
+
+int main() {
+    long B = 0, A = 0;
+
+    while (cin >> A >> B) {
+        // your stuff here
+        cout << A - B << endl;
+    }
+    return 0;
+}
