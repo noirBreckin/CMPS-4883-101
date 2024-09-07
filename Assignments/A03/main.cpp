d7052c47e04f0667375b61ce1c4f1b80bc569d28
noirBreckin <112154255+noirBreckin@users.noreply.github.com>
2024-09-06 19:44:00 -0500

    Update main.cpp

--- Assignments/A03/main.cpp
+++ Assignments/A03/main.cpp
@@ -4,17 +4,50 @@
 * Date: 9-6-24
 */
 #include <iostream>
-
+#include <fstream>
 #define endl "\n"
 
 using namespace std;
 
+
+void openFiles(ifstream &infile, ofstream &outfile);
+
 int main() {
+    ifstream input;
+    ofstream output;
+    openFiles(input,output);
+  
     long B = 0, A = 0;
 
-    while (cin >> A >> B) {
-        // your stuff here
-        cout << A - B << endl;
-    }
-    return 0;
+    while(input >> A >> B)
+      {
+        if(A > B)
+        {
+          cout << A - B << endl;
+          output << A - B << endl;
+        }
+        else
+        {
+          cout << B - A << endl;
+          output << B - A << endl;
+        }
+
+      }
+  input.close();
+  output.close();
+  return 0;
+}
+
+// Implementation for openFiles
+void openFiles(ifstream &infile, ofstream &outfile) {
+  char inFileName[40];
+  char outFileName[40];
+
+  cout << "Enter the input file name please: " << endl;
+  cin >> inFileName;
+  infile.open(inFileName); // Opens the input file
+
+  cout << "Enter the output file name please: " << endl;
+  cin >> outFileName;
+  outfile.open(outFileName); // open output file
 }
